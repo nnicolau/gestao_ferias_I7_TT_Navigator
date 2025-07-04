2508090e44a811ceea2f7e9628b2ba2176394db4
nnicolau <nmrnicolau@gmail.com>
2025-07-04 18:00:21 +0100

    Update gestao_ferias.py

--- gestao_ferias.py
+++ gestao_ferias.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime
 import sqlite3
 from sqlite3 import Error
+import matplotlib.pyplot as plt
 
 # Configuração inicial
 st.set_page_config(page_title="Gestão de Férias", layout="wide")
