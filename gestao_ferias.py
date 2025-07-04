c6e3c93fb4a5e55bb0d3fb6d2827e220979ea4c2
nnicolau <nmrnicolau@gmail.com>
2025-07-04 19:18:06 +0100

    Update gestao_ferias.py

--- gestao_ferias.py
+++ gestao_ferias.py
@@ -328,6 +328,7 @@ with tab2:
                     st.info("Nenhuma férias marcada para editar.")
         else:
             st.warning("Nenhum funcionário cadastrado. Cadastre funcionários primeiro.")            
+   
     with tab3:
     st.header("Consultas e Relatórios")
     
